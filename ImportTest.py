0603e2c2340b67f1dd13fc9eabe6af47cca00f7b
Thatjester <thatjester@gmail.com>
2018-09-04 17:02:36 -0500

    First commit

---
+++ ImportTest.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+
+    print(name)
+    print(score)
+    
+    nameScore = [name] + [score]
+    
+
+    
+    print(nameScore).sort()
